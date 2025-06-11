f063d719c0b07982f4bbc81ea9870a3710d51546
Adibayu Luthfiansyah Setyawan <106433930+Adibayuluthfiansyah@users.noreply.github.com>
2025-06-11 23:25:56 +0700

    make customers and users page

---
+++ my-next-js/src/app/customer/page.tsx
@@ -0,0 +1,9 @@
+import styles from "./customerPage.module.css"
+
+const Customers = () => {
+  return (
+    <div className={styles.bgTomato}>CUSTOMERS PAGE</div>
+  )
+}
+
+export default Customers
